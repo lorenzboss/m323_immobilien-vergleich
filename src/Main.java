28a36b5934473deae8fa6e761fe245997e6a9c28
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-09-10 23:44:46 +0200

    Import all nine test functions in the main method and execute all of them with the data from the JsonToImmobilien converter.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import immobilien.Immobilien;
+import immobilien.JsonToImmobilien;
+import logic.TestFunctions;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
+        final List<Immobilien> immobilienList = JsonToImmobilien.convertJsonToImmobilien("dataset.json");
+
+        TestFunctions.anzahlImmobilien(immobilienList);
+        TestFunctions.immobilienAusgeben(immobilienList);
+        TestFunctions.immobilienAusgebenKompakt(immobilienList);
+        TestFunctions.verkaufspreisSechzehnTeuersteImmobilien(immobilienList);
+        TestFunctions.sechzehnTeuersteImmobilien(immobilienList);
+        TestFunctions.anzahlImmobilienProBezirk(immobilienList);
+        TestFunctions.anzahlImmobilienProBezirkSortiert(immobilienList);
+        TestFunctions.durchschnittspreisProZimmer(immobilienList);
+        TestFunctions.durchschnittspreisProZimmerSortiert(immobilienList);
+
     }
 }
