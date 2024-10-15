5fec8fcb49ffe0df63a6d18aaacebf50c7db37d5
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-10-15 09:16:31 +0200

    Create the console application for the PrintProperties.

--- src/userInteraction/PrintProperties.java
+++ src/userInteraction/PrintProperties.java
@@ -1,12 +1,46 @@
 package userInteraction;
 
+import static userInteraction.HelperMethods.askForInt;
+import static userInteraction.HelperMethods.askYesNoQuestion;
+
 import java.util.List;
 import properties.Property;
 
 public class PrintProperties {
+
   public static void start(List<Property> propertyList, LogicOption logicOption) {
-    System.out.println("Printing all properties");
+    boolean compactForm =
+        askYesNoQuestion("Do you want to print the properties in a compact form? (y/n): ");
+    boolean setLimit = askYesNoQuestion("Do you want to set a limit? (y/n): ");
+    Integer limit = setLimit ? askForInt("Please enter the limit:", propertyList.size()) : null;
 
+    printProperties(propertyList, logicOption, compactForm, limit);
+  }
 
+  private static void printProperties(
+      List<Property> propertyList, LogicOption logicOption, boolean compactForm, Integer limit) {
+    if (compactForm) {
+      printPropertiesCompactForm(propertyList, logicOption, compactForm, limit);
+    } else {
+      printPropertiesNormalForm(propertyList, logicOption, compactForm, limit);
+    }
+  }
+
+  private static void printPropertiesCompactForm(
+      List<Property> propertyList, LogicOption logicOption, boolean compactForm, Integer limit) {
+    if (logicOption == LogicOption.FUNCTIONAL) {
+      logic.functional.PrintProperties.printPropertiesCompact(propertyList, limit);
+    } else {
+      logic.imperative.PrintProperties.printPropertiesCompact(propertyList, limit);
+    }
+  }
+
+  private static void printPropertiesNormalForm(
+      List<Property> propertyList, LogicOption logicOption, boolean compactForm, Integer limit) {
+    if (logicOption == LogicOption.FUNCTIONAL) {
+      logic.functional.PrintProperties.printProperties(propertyList, limit);
+    } else {
+      logic.imperative.PrintProperties.printProperties(propertyList, limit);
+    }
   }
 }
