85d7ca7b80d0b7bb9f9d2fec4d3eddf5eeae3020
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-10-18 22:18:59 +0200

    Write the PrintHighestPrice class.

--- src/userInteraction/PrintHighestPrice.java
+++ src/userInteraction/PrintHighestPrice.java
@@ -1,12 +1,44 @@
 package userInteraction;
 
-import java.util.List;
+import static userInteraction.HelperMethods.askForInt;
+import static userInteraction.HelperMethods.askYesNoQuestion;
 
+import java.util.List;
 import properties.Property;
 
 public class PrintHighestPrice {
-    public static void start(List<Property> propertyList, LogicOption logicOption) {
-        System.out.println("Printing the most expensive properties");
-    }
+  public static void start(List<Property> propertyList, LogicOption logicOption) {
+    boolean priceOnly =
+        askYesNoQuestion("Do you only want to print out the price of the properties? (y/n): ");
+    int limit = askForInt("How many properties do you want to print:", propertyList.size());
 
+    printHighestPrice(propertyList, logicOption, priceOnly, limit);
+  }
+
+  private static void printHighestPrice(
+      List<Property> propertyList, LogicOption logicOption, boolean priceOnly, int limit) {
+    if (priceOnly) {
+      printHighestPricePriceOnly(propertyList, logicOption, limit);
+    } else {
+      printHighestPriceNormalForm(propertyList, logicOption, limit);
+    }
+  }
+
+  private static void printHighestPricePriceOnly(
+      List<Property> propertyList, LogicOption logicOption, int limit) {
+    if (logicOption == LogicOption.FUNCTIONAL) {
+      logic.functional.HighestPrice.mostExpensivePropertiesPrice(propertyList, limit);
+    } else {
+      logic.imperative.HighestPrice.mostExpensivePropertiesPrice(propertyList, limit);
+    }
+  }
+
+  private static void printHighestPriceNormalForm(
+      List<Property> propertyList, LogicOption logicOption, int limit) {
+    if (logicOption == LogicOption.FUNCTIONAL) {
+      logic.functional.HighestPrice.mostExpensiveProperties(propertyList, limit);
+    } else {
+      logic.imperative.HighestPrice.mostExpensiveProperties(propertyList, limit);
+    }
+  }
 }
